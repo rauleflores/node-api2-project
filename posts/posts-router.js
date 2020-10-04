a861ef61ba1fd706d658ea86535b9328440cc1c9
Raul <raulefloresjr@gmail.com>
2020-10-04 00:29:44 -0700

    endpoints GET /api/posts/:id/comments & POST /api/posts are working correctly

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -36,4 +36,43 @@ router.get("/api/posts/:id", (req, res) => {
     });
 });
 
+router.get("/api/posts/:id/comments", (req, res) => {
+  const id = req.params.id;
+  db.findPostComments(id)
+    .then((comments) => {
+      if (comments.length !== 0) {
+        res.status(200).json(comments);
+      } else {
+        res.status(404).json({
+          errorMessage: "The post with the specified ID does not exist.",
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        errorMessage: "The comments could not be retrieved.",
+      });
+    });
+});
+
+router.post("/api/posts", (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    return res.status(400).json({
+      errorMessage: "Please provide title and contents for the post.",
+    });
+  }
+  db.insert({ title, contents })
+    .then((post) => {
+      res.status(201).json(post);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        errorMessage: "Could not create a post.",
+      });
+    });
+});
+
 module.exports = router;
